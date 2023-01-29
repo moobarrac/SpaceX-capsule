a9a0cba648bd745f7b3f7a283726fc7a0f814ed2
moobarrac <wahabajibola01@gmail.com>
2023-01-29 22:00:55 +0100

    add click function to previous and next button

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { SpaceContext } from '../context/ContextProvider';
 
 const Pagination = () => {
+  const { offset, totalItem, limit, loadMore } = useContext(SpaceContext);
+
   return (
     <div className="bg-black container mx-auto py-8 text-white">
       <div className="flex justify-center gap-2">
         <button
-          className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l`}
+          className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-l ${(offset === 0) && "cursor-not-allowed opacity-50"}`}
+          onClick={() => loadMore('prev')}
         >
           Previous
         </button>
         <button
-          className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r`}
+          className={`bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded-r ${(offset + limit > totalItem) && "cursor-not-allowed opacity-50"}`}
+          onClick={() => loadMore('next')}
         >
           Next
         </button>
