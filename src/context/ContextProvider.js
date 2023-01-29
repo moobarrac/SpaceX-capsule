6eac81359b7cc7646488c72329acafcb192603ad
moobarrac <wahabajibola01@gmail.com>
2023-01-29 20:21:47 +0100

    add filter and setfilters to context

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -3,9 +3,10 @@ import React, { useState } from 'react';
 const SpaceContext = React.createContext();
 
 const SpaceProvider = ({children}) => {
+  const [filters, setFilters] = useState({status: '', date: '', type: ''})
   
   return (
-    <SpaceContext.Provider value={{}}>
+    <SpaceContext.Provider value={{filters, setFilters}}>
       {children}
     </SpaceContext.Provider>
   );
