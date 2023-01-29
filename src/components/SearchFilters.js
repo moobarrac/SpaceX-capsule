ff03afe31a0c6435feb29404750eca32dfa2f22e
moobarrac <wahabajibola01@gmail.com>
2023-01-29 20:33:06 +0100

    add handleChange function for all input to get values

--- src/components/SearchFilters.js
+++ src/components/SearchFilters.js
@@ -2,16 +2,31 @@ import React, { useContext } from "react";
 import { SpaceContext } from "../context/ContextProvider";
 
 const SearchFilters = () => {
-  const {filters, setFilters} = useContext(SpaceContext)
+  const {filters, setFilters, handleSubmit} = useContext(SpaceContext)
+
+  const handleInputChange = e => {
+    if(e.target.name === "date") {
+      let date = new Date(e.target.value).toISOString()
+      setFilters({
+        ...filters,
+        date
+      });
+    }
+    setFilters({
+      ...filters,
+      [e.target.name]: e.target.value
+    });
+  }
 
   return (
     <div className="p-4 flex flex-wrap gap-2 mt-4 mb-8 justify-center" data-testid="search">
       <div className="mb-2">
         <select 
           className="bg-white border-2 border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-tight focus:outline-none focus:bg-white"
+          value={filters.status}
           name="status"
           placeholder="Select status"
-        >
+          onChange={handleInputChange}>
           <option value="">All</option>
           <option value="active">Active</option>
           <option value="destroyed">Destroyed</option>
@@ -25,19 +40,24 @@ const SearchFilters = () => {
           type="date"
           name="date"
           data-testid="date-input"
+          value={filters.date}
+          onChange={handleInputChange}
         />
       </div>
       <div className="mb-2">
         <input 
           className="bg-white border-2 border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-tight focus:outline-none focus:bg-white"
           type="text"
+          value={filters.type}
           placeholder="Input type"
           name="type"
           data-testid="type-input"
+          onChange={handleInputChange}
         />
       </div>
       <button
         className="bg-blue-900 text-white rounded-lg py-2 px-4 hover:bg-blue-700"
+        onClick={() => handleSubmit()}
         data-testid="search-button"
       >
         Search
